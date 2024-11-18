b9ecbe9949c13093224b19ff8259f6ce90fa325b
harD1kkkk <137618635+harD1kkkk@users.noreply.github.com>
2024-11-18 15:09:29 +0200

    Update lab_6.3_rec.cpp

--- LAB_6.3_rec/LAB_6.3_rec/lab_6.3_rec.cpp
+++ LAB_6.3_rec/LAB_6.3_rec/lab_6.3_rec.cpp
@@ -1,32 +1,21 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-int CountEvenElements(int* a, const int size, int i);
-template <typename T>
-int CountEvenElements(T* a, const int size, int i);
-
-int main() {
-    const int n = 10;
-    int a[n] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-    int count1 = CountEvenElements(a, n, 0);
-    cout << "CountEvenElements(a, n, 0) = " << count1 << endl;
-
-    int count2 = CountEvenElements<int>(a, n, 0);
-    cout << "CountEvenElements<int>(a, n, 0) = " << count2 << endl;
-
-    return 0;
-}
-
-int CountEvenElements(int* a, const int size, int i) {
+int CountEvenElements(int* a, const int size, int i = 0) {
     if (i >= size) {
         return 0;
     }
-    return (a[i] % 2 == 0 ? 1 : 0) + CountEvenElements(a, size, i + 1);
+    if (a[i] % 2 == 0) {
+        return 1 + CountEvenElements(a, size, i + 1);
+    }
+    else {
+        return CountEvenElements(a, size, i + 1);
+    }
 }
 
 template <typename T>
-int CountEvenElements(T* a, const int size, int i) {
+int CountEvenElements(T* a, const int size, int i = 0) {
     if (i >= size) {
         return 0;
     }
@@ -36,4 +25,63 @@ int CountEvenElements(T* a, const int size, int i) {
     else {
         return CountEvenElements<T>(a, size, i + 1);
     }
+}
+
+void CreateArrayWithRandomElements(int* a, int numberOfElements, int start, int end, int i = 0) {
+    if (i < numberOfElements) {
+        a[i] = start + rand() % (end - start + 1);
+        CreateArrayWithRandomElements(a, numberOfElements, start, end, i + 1);
+    }
+}
+
+template <typename T>
+void CreateArrayWithRandomElements(T* a, int numberOfElements, int start, int end, int i = 0) {
+    if (i < numberOfElements) {
+        a[i] = start + rand() % (end - start + 1);
+        CreateArrayWithRandomElements<T>(a, numberOfElements, start, end, i + 1);
+    }
+}
+
+void PrintArray(int* a, int size, int i = 0) {
+    if (i < size) {
+        cout << "a[" << setw(2) << i << " ] = " << setw(4) << a[i] << endl;
+        PrintArray(a, size, i + 1);
+    }
+}
+
+template <typename T>
+void PrintArray(T* a, int size, int i = 0) {
+    if (i < size) {
+        cout << "a[" << setw(2) << i << " ] = " << setw(4) << a[i] << endl;
+        PrintArray<T>(a, size, i + 1);
+    }
+}
+
+int main() {
+    srand((unsigned)time(NULL));
+
+    const int size = 10;
+    int a[size];
+
+    cout << "Create Array" << endl;
+    CreateArrayWithRandomElements(a, size, -20, 34, 0);
+
+    cout << "Print" << endl;
+    PrintArray(a, size, 0);
+
+    int count1 = CountEvenElements(a, size);
+    cout << "CountEvenElements (function) = " << count1 << endl;
+
+    cout << endl;
+
+    cout << "Create Array(template)" << endl;
+    CreateArrayWithRandomElements<int>(a, size, -20, 34, 0);
+
+    cout << "Print(template)" << endl;
+    PrintArray<int>(a, size, 0);
+
+    int count2 = CountEvenElements<int>(a, size);
+    cout << "CountEvenElements (template) = " << count2 << endl;
+
+    return 0;
 }
